41509e6c3696d4e604b941de6871eef309194e03
dylanperer <dylan.perer29@protonmail.com>
2023-10-30 19:49:33 +1300

    tailwind/nativewind css transpile

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+
+// Ensure we import the CSS for Tailwind so it's included in hot module reloads.
+//@ts-ignore
+const ctx = require.context(
+    // If this require.context is not inside the root directory (next to the package.json) then adjust this file path
+    // to resolve correctly.
+    './node_modules/.cache/expo/tailwind'
+);
+if (ctx.keys().length) ctx(ctx.keys()[0]);
 
 export default function App() {
   return (
