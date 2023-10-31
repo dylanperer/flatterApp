626f2a5472470a1a66cad8e0f7f3565b3aab7d45
dylanperer <dylan.perer29@protonmail.com>
2023-10-31 21:52:50 +1300

    reanimated svg

--- lib/common/components/AppInputField.tsx
+++ lib/common/components/AppInputField.tsx
@@ -2,13 +2,10 @@ import React, { useRef } from 'react';
 import { TextInput, TouchableOpacity, Text, View, Image } from 'react-native';
 import EmailIcon from '../icons/EmailIcon';
 import { AppColors } from '../../utils/constants/AppColors';
-import { useAnimationState } from 'moti';
 import Animated, {
-   useAnimatedStyle,
+   useAnimatedProps,
    useSharedValue,
-   withRepeat,
-   withSpring,
-   withTiming,
+   withTiming
 } from 'react-native-reanimated';
 
 interface IAppInputField extends Partial<TextInput>, IAppComponent {
@@ -18,7 +15,6 @@ interface IAppInputField extends Partial<TextInput>, IAppComponent {
 export const AppInputField: React.FC<IAppInputField> = ({ props }) => {
    const textInputRef = useRef(null);
 
-   const AnimatedTextInput = Animated.createAnimatedComponent(EmailIcon);
    const deltaColor = useSharedValue<string>(AppColors.stone[500]);
 
    const onFocus = () => {
@@ -29,11 +25,15 @@ export const AppInputField: React.FC<IAppInputField> = ({ props }) => {
       deltaColor.value = withTiming(AppColors.stone[500], { duration: 300 });
    };
 
+   const animatedProps = useAnimatedProps(() => ({
+      fill: deltaColor.value,
+   }));
+
    return (
       <TouchableOpacity className={'h-8 flex flex-col justify-between items-center w-full'}
                         onPress={() => textInputRef.current.focus()}>
          <View className={'h-8 flex flex-row justify-between items-center w-full gap-2'}>
-            <AnimatedTextInput width={22} height={22} fill={deltaColor} />
+            <EmailIcon animatedProps={animatedProps}  width={22} height={22} />
             <TextInput className='flex-1 h-full'
                        placeholder={props.placeholder}
                        ref={textInputRef}
